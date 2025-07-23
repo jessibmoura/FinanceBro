79a3e5a7d4735d89b3e48def10c0f4b662f4718f
jessibmoura <jessicabmoura00@gmail.com>
2025-07-22 20:32:07 -0400

    started to parse expense message and return to user

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from utils.utils import *
+
 import telebot
 from dotenv import load_dotenv
 import os
@@ -12,8 +14,8 @@ bot = telebot.TeleBot(TOKEN) # type: ignore
 def first_contact(message):
     """ Asks user for name and welcomes them """
     chat_id = message.chat.id
-    bot.send_message(chat_id, """Hey there! I'm Finance Bro and my objective is to help you with your finance life. I hope you and I can do a great work together! :)\n
-                     First things first, how can I call you?""")
+    bot.send_message(chat_id, """What’s up, legend? Name’s Finance Bro — your personal CFO, hype man, and accountability partner on the road to obscene wealth.\n 
+I’m here to 10x your net worth, optimize your hustle, and make sure your wallet stays as jacked as your gym routine.\n\nBut before we start stacking wins... what should I call the next big player in this market?""")
     bot.register_next_step_handler(message,welcoming)
 
 def welcoming(message):
@@ -21,14 +23,34 @@ def welcoming(message):
     chat_id = message.chat.id
     name = message.text.strip()
     users[chat_id] = name
-    bot.send_message(chat_id,f""" Nice,{name}! Let's save some money bro 🤖💰""")
+    bot.send_message(chat_id,f""" Lock it in, {name}.\nWe’re about to dominate this savings game like it’s earnings season.\nNo lattes, no liabilities — just pure, uncut capital gains. Let’s build that war chest, bro 💼🔥""")
 
 
 @bot.message_handler(commands=['hello'])
 def hello(message):
     """ Message asking if user needs help """
     chat_id = message.chat.id
-    bot.send_message(chat_id, f"Yo {users[chat_id]}, how can i help you bro?")
+    bot.send_message(chat_id, f"Yo {users[chat_id]}, talk to me. What’s the play, bro?")
+
+@bot.message_handler(commands=['in'])
+def expense(message):
+    """ Message to register a new income value """
+    chat_id = message.chat.id
+    bot.send_message(chat_id, f"Logging a new capital outflow, bro? What are we tagging this one as? ")
+    bot.register_next_step_handler(message, process_expense)
+
+def process_expense(message):
+    """ Extracts the information necessary from the message """
+    chat_id = message.chat.id
+    expense_message = message.text.strip() # <expense: float> blank-space <description: str optional> blank-space <category: str>
+
+    try:
+        float_value, description, category = parse_message(expense_message)
+        bot.send_message(chat_id, f"So, what I'm getting is:\n\nValue: {float_value}\nDescription: {description}\nCategory: {category}\n\nAm I wrong? [Y/N]")
+    except Exception as e:
+        bot.send_message(chat_id, f"Oops! There was an error processing your input: \n>{e}\nPlease try again.")
+        bot.register_next_step_handler(message, process_expense)  # tenta de novo
+
 
 print("Bot running...")
 bot.infinity_polling()
---
+++ utils/utils.py
@@ -0,0 +1,28 @@
+def parse_message(msg: str):
+    """
+    Functions takes the expense message and extracts three values from it: the float value 
+    for the expense, the description - which is optional - and the category for the expense. 
+    Both value and category are necessary.
+
+    Parameters
+    ----------
+    msg: str
+        Expense message sent to the bot
+    """
+    lines = [line.strip() for line in msg.strip().split('\n') if line.strip()]
+    
+    if len(lines) == 3:
+        str_value, description, category = lines
+    elif len(lines) == 2:
+        str_value, category = lines
+        description = None  # value None if no description is given
+    else:
+        raise ValueError("Invalid format: Use:\n<value>\n[description: optional]\n<category>")
+
+    # Validação e conversão do valor
+    try:
+        float_value = float(str_value.replace(',', '.'))
+    except ValueError:
+        raise ValueError("Invalid value: Please only use numbers for the values.")
+
+    return float_value, description, category
